9f2f70075009a53621e55287c30e1b39d37b5de9
MungaiKeren <wambukeren@gmail.com>
2024-08-12 15:24:11 +0300

    Refactor Create page to include form and button in Create.js

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,34 +1,82 @@
-import React from "react";
-import { 
-    Container,
-    Typography,
-    Button
-} from "@mui/material";
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
-
+import React, { useState } from "react";
+import { Container, Typography, Button, TextField } from "@mui/material";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
 export default function Create() {
-    return (
-        <Container>
-            <Typography 
-                variant="h6"
-                component="h2"
-                gutterBottom
-                color="textSecondary"
-            >
-                Create a new note
-            </Typography>
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title === "") {
+      setTitleError(true);
+    }
+    if (details === "") {
+        setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details);      
+    }
+  };
 
 
-            <Button 
-                variant="contained"
-                type="submit"
-                color="secondary"
-                startIcon={<SendOutlinedIcon />}
-                onClick={() => console.log("Click")}
-            >
-                Send
-            </Button>
-        </Container>
-    );
-};
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a new note
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          sx={{
+            marginTop: 2,
+            marginBottom: 2,
+            display: "block",
+          }}
+          error={titleError}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <TextField
+          label="Note Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          sx={{
+            marginBottom: 2,
+            display: "block",
+          }}
+          error={detailsError}
+          onChange={(e) => setDetails(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          type="submit"
+          color="secondary"
+          endIcon={<SendOutlinedIcon />}
+          onClick={() => console.log("Click")}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+}
