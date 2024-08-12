62b75bdb60df399614a8581fadba79a5edd690bd
MungaiKeren <wambukeren@gmail.com>
2024-08-12 11:07:14 +0300

    Add Create page with form and button

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { 
+    Container,
+    Typography,
+    Button
+} from "@mui/material";
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+
 
 export default function Create() {
     return (
-        <div>
-            <h1>Create</h1>
-        </div>
+        <Container>
+            <Typography 
+                variant="h6"
+                component="h2"
+                gutterBottom
+                color="textSecondary"
+            >
+                Create a new note
+            </Typography>
+
+
+            <Button 
+                variant="contained"
+                type="submit"
+                color="secondary"
+                startIcon={<SendOutlinedIcon />}
+                onClick={() => console.log("Click")}
+            >
+                Send
+            </Button>
+        </Container>
     );
 };
