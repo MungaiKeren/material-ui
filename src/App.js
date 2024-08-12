a636ef8609d2002c84a88a5d0fb421f00f072ef6
MungaiKeren <wambukeren@gmail.com>
2024-08-12 14:50:18 +0300

    Add MUI theme and update App.js and index.css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Notes from "./Pages/Notes";
 import Create from "./Pages/Create";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe"
+    },
+    secondary: {
+      main: "#9c27b0"
+    }
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Notes />} />
-        <Route path="/create" element={<Create />}/>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="/create" element={<Create />}/>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
