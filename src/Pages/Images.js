04df5e45b09dee75a02c261fbac1d231bd1091d3
MungaiKeren <wambukeren@gmail.com>
2024-08-13 16:43:21 +0300

    Update Images.js to include ImageListItemBar and IconButton components

--- src/Pages/Images.js
+++ src/Pages/Images.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Container } from '@mui/material';
-import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import { Info } from '@mui/icons-material';
 
 export default function ImageMasonry() {
   return (
@@ -16,6 +18,18 @@ export default function ImageMasonry() {
               alt={item.title}
               loading="lazy"
             />
+            <ImageListItemBar 
+                title={item.title} 
+                subtitle={item.author} 
+                actionIcon={
+                    <IconButton
+                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                        aria-label={`info about ${item.title}`}
+                    >
+                        <Info />
+                    </IconButton>
+                }
+            />
           </ImageListItem>
         ))}
       </ImageList>
@@ -27,49 +41,83 @@ const itemData = [
   {
     img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
     title: 'Bed',
+    author: '@bkristastucchio',
   },
   {
     img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
     title: 'Books',
+    author: '@rpnickson',
   },
   {
     img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
     title: 'Sink',
+    author: '@arwinneil',
   },
   {
     img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
     title: 'Kitchen',
+    author: '@helloimnik',
   },
   {
     img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
     title: 'Blinds',
+    author: '@nicholasbui',
   },
   {
     img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
     title: 'Chairs',
+    author: '@rpnickson',
   },
   {
     img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
     title: 'Laptop',
+    author: '@hckmstrm',
   },
   {
     img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
     title: 'Doors',
+    author: '@derektxu',
   },
   {
     img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
     title: 'Coffee',
+    author: '@doyouwantmoore',
   },
   {
     img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
     title: 'Storage',
+    author: '@nicholasbui',
   },
   {
     img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
     title: 'Candle',
+    author: '@tanelah',
   },
   {
     img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
     title: 'Coffee table',
+    author: '@oneeightystudio',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+    author: '@shelleypauls',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+    author: '@peterlaster',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+    author: '@katie_wasserman',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+    author: '@silverdalex',
+    rows: 2,
+    cols: 2,
   },
 ];
