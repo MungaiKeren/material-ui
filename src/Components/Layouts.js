f7ca15be7f85af15bbaea668be08ea1a1bf7b198
MungaiKeren <wambukeren@gmail.com>
2024-08-13 15:05:30 +0300

    padding

--- src/Components/Layouts.js
+++ src/Components/Layouts.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { 
     Drawer,
     Typography,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText
 } from "@mui/material";
+import { AddCircleOutline, SubjectOutlined } from "@mui/icons-material";
 
 const drawerWidth = 240;
 
 export default function Layout({ children }) {
+    const location = window.location.pathname;
+    console.log(location);
+
+    const menuItems = [
+        {
+            text: "My Notes",
+            icon: <SubjectOutlined color="secondary" />,
+            path: "/"
+        },
+        {
+            text: "Create Note",
+            icon: <AddCircleOutline color="secondary"/>,
+            path: "/create"
+        }
+    ]
+
     return (
         <div style={{display: "flex"}}>
             <Drawer
@@ -21,9 +42,31 @@ export default function Layout({ children }) {
                     },
                 }}
             >
-                <Typography>
+                <Typography
+                    sx={{
+                        padding: 2
+                    }}
+                    letterSpacing={8}
+                >
                     Ninja Notes
                 </Typography>
+
+                {/* list items */}
+                <List>
+                    {menuItems.map((item, index) => (
+                        <ListItem 
+                            key={index}
+                            button                            
+                            onClick={() => window.location.href = item.path}
+                            sx={location == item.path ? {
+                                background: "#B1AFFF"
+                            } : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText>{item.text}</ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
             </Drawer>
             <div>
                 {children}
